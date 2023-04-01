9749f59ecd8e253bdb7125d55d3709d1a47de6aa
Ayse Bat <110904026+aysebat@users.noreply.github.com>
2023-04-01 11:07:16 +0000

    create_multiple_pdf_from_excel_data function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fpdf import FPDF
-
+import pandas as pd
 
 def create_pdf_document():
   #emty pdf document
@@ -35,4 +35,64 @@ def create_pdf_document():
   pdf.cell(w=100,h=25, txt='Chordata', ln=1)
   
   pdf.output('output.pdf')
-create_pdf_document()
+#create_pdf_document()
+
+#Let's crete multiple pdf document from the excel data
+def create_multiple_pdf_from_excel_data():
+  df  = pd.read_excel('data.xlsx')
+  
+  for index, row in df.iterrows():
+    pdf = FPDF(orientation='P', unit='pt', format='A4')
+    pdf.add_page()
+  
+    #create title
+    pdf.set_font(family='Times', style='B', size=24)
+    pdf.cell(w=0, h=50, txt=row['name'], align='C', ln=1)
+  
+    #kingdom
+    pdf.set_font(family='Times', style='B', size=14)
+    pdf.cell(w=100,h=25, txt='Kingdom: ')
+    
+    pdf.set_font(family='Times', size=14)
+    pdf.cell(w=100,h=25, txt=row['kingdom'], ln=1)
+  
+    #phylum
+    pdf.set_font(family='Times', style='B', size=14)
+    pdf.cell(w=100,h=25, txt='Phylum: ')
+    
+    pdf.set_font(family='Times', size=14)
+    pdf.cell(w=100,h=25, txt=row['phylum'], ln=1)
+  
+    #class
+    pdf.set_font(family='Times', style='B', size=14)
+    pdf.cell(w=100,h=25, txt='Class: ')
+    
+    pdf.set_font(family='Times', size=14)
+    pdf.cell(w=100,h=25, txt=row['class'], ln=1)
+  
+    #order
+    pdf.set_font(family='Times', style='B', size=14)
+    pdf.cell(w=100,h=25, txt='Order: ')
+    
+    pdf.set_font(family='Times', size=14)
+    pdf.cell(w=100,h=25, txt=row['order'], ln=1)
+  
+    #suborder
+    pdf.set_font(family='Times', style='B', size=14)
+    pdf.cell(w=100,h=25, txt='Suborder: ')
+    
+    pdf.set_font(family='Times', size=14)
+    pdf.cell(w=100,h=25, txt=row['suborder'], ln=1)
+  
+    pdf.output(f"{row['name']}.pdf")
+    
+#create_multiple_pdf_from_excel_data()
+  
+
+
+
+
+
+  
+
+
